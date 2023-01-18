fc3dfa74d32f843aaeb44cfb3e39a42714e55b8b
dominicmkennedy <dominicmkennedy@gmail.com>
2023-01-18 15:43:07 -0500

    Change all array to Vec; no more constexper sizes

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 const UNIVERSE_WIDTH: usize = 16;
 const MAX_TIME_STEPS: usize = 32;
 const NUM_CELL_TYPES: u8 = 10;
-const RULE_TABLE_WIDTH: usize = (((NUM_CELL_TYPES as usize) - 1) * 3) + 1;
 const COLORS: [[f32; 4]; 10] = [
     [0.0, 0.0, 0.0, 1.0],
     [0.0, 1.0, 0.0, 1.0],
@@ -15,34 +14,26 @@ const COLORS: [[f32; 4]; 10] = [
     [0.7, 0.1, 0.2, 1.0],
 ];
 
-const TOTAL_CELLS: usize = UNIVERSE_WIDTH * MAX_TIME_STEPS;
-const NUM_VERTS: usize = TOTAL_CELLS * 2 * 3 * 2;
-const NUM_COLORS: usize = NUM_VERTS * 2;
+mod utils;
 
-use js_sys::Math::random;
-use js_sys::WebAssembly;
-use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
+use js_sys::{Math::random, WebAssembly};
+use wasm_bindgen::{prelude::*, JsCast};
 use web_sys::{WebGl2RenderingContext, WebGlProgram, WebGlShader};
 
-// generate initial cells
-fn gen_init_cells<const SIZE: usize>(n: u8) -> [u8; SIZE] {
-    let mut arr = [0; SIZE];
-    for x in &mut arr {
-        *x = gen_range(0, n);
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
     }
-    arr
 }
 
-// generate rule table
-// try and combine this with init_cells fn
-fn gen_rule_table<const SIZE: usize>(n: u8) -> [u8; SIZE] {
-    let mut arr = [0; SIZE];
-    for x in &mut arr {
-        *x = gen_range(1, n);
-    }
-    arr[0] = 0;
-    arr
+fn gen_init_cells(n: u8, width: usize) -> Vec<u8> {
+    vec![0; width].iter().map(|&_| gen_range(0, n)).collect()
+}
+
+fn gen_rule_table(n: u8, width: usize) -> Vec<u8> {
+    let mut table: Vec<u8> = vec![0; width].iter().map(|&_| gen_range(1, n)).collect();
+    table[0] = 0;
+    table
 }
 
 // generate a random number within a range
@@ -53,12 +44,8 @@ fn gen_range(low: u8, high: u8) -> u8 {
 
 // next step function
 // consider using a more functional approch for the new state
-fn next_generation<const RULE_TABLE_WIDTH: usize, const UNIVERSE_WIDTH: usize>(
-    rule_table: &[u8; RULE_TABLE_WIDTH],
-    old_state: &[u8; UNIVERSE_WIDTH],
-    new_state: &mut [u8; UNIVERSE_WIDTH],
-) {
-    let len = old_state.len();
+fn next_generation(rule_table: &Vec<u8>, old_state: &Vec<u8>, new_state: &mut Vec<u8>) {
+    let len = new_state.len();
 
     for i in 0..len {
         let tot =
@@ -67,31 +54,32 @@ fn next_generation<const RULE_TABLE_WIDTH: usize, const UNIVERSE_WIDTH: usize>(
     }
 }
 
-fn ca<const NUM_COLORS: usize>() -> [f32; NUM_COLORS] {
+fn ca(universe_width: usize, max_time_steps: usize, num_cell_types: u8) -> Vec<f32> {
+    let rule_table_width: usize = (((num_cell_types as usize) - 1) * 3) + 1;
+
     // rule table
-    let rule_table: [u8; RULE_TABLE_WIDTH] = gen_rule_table(NUM_CELL_TYPES);
+    let rule_table = gen_rule_table(num_cell_types, rule_table_width);
 
     // initial cells
-    let init_cells: [u8; UNIVERSE_WIDTH] = gen_init_cells(NUM_CELL_TYPES);
+    let init_cells = gen_init_cells(num_cell_types, universe_width);
 
     // blank cell board
-    let mut cell_board: [[u8; UNIVERSE_WIDTH]; MAX_TIME_STEPS] =
-        [[0; UNIVERSE_WIDTH]; MAX_TIME_STEPS];
+    let mut cell_board = vec![vec![0; universe_width]; max_time_steps];
 
     // advance time steps
-    let mut last_row = &init_cells;
+    let mut prev_row = &init_cells;
     for next_row in cell_board.iter_mut() {
-        next_generation(&rule_table, last_row, next_row);
-        last_row = next_row;
+        next_generation(&rule_table, prev_row, next_row);
+        prev_row = next_row;
     }
 
     // return results as verts and colors
     cell_colors(&cell_board)
 }
 
-// rewrite better, this is hidious and wrong
-fn cell_verts<const NUM_VERTS: usize>() -> [f32; NUM_VERTS] {
-    let mut vertices: [f32; NUM_VERTS] = [0.0; NUM_VERTS];
+// rewrite better, this is hidious
+fn cell_verts(num_verts: usize) -> Vec<f32> {
+    let mut vertices = vec![0.0; num_verts];
 
     const CELL_WIDTH: f32 = 2.0 / (UNIVERSE_WIDTH as f32);
     const CELL_HEIGHT: f32 = 2.0 / (MAX_TIME_STEPS as f32);
@@ -130,14 +118,12 @@ fn cell_verts<const NUM_VERTS: usize>() -> [f32; NUM_VERTS] {
 }
 
 // this function is an absolute disaster
-fn cell_colors<
-    const MAX_TIME_STEPS: usize,
-    const UNIVERSE_WIDTH: usize,
-    const NUM_COLORS: usize,
->(
-    cell_board: &[[u8; UNIVERSE_WIDTH]; MAX_TIME_STEPS],
-) -> [f32; NUM_COLORS] {
-    let mut colors: [f32; NUM_COLORS] = [0.0; NUM_COLORS];
+fn cell_colors(cell_board: &Vec<Vec<u8>>) -> Vec<f32> {
+    let total_cells: usize = cell_board.len() * cell_board[0].len();
+    let num_verts: usize = total_cells * 2 * 3 * 2;
+    let num_colors: usize = num_verts * 2;
+
+    let mut colors = vec![0.0; num_colors];
 
     for i in 0..cell_board.len() {
         for j in 0..cell_board[i].len() {
@@ -181,6 +167,8 @@ fn cell_colors<
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
+    utils::set_panic_hook();
+
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
@@ -232,8 +220,11 @@ pub fn start() -> Result<(), JsValue> {
     let vert_buffer = gl.create_buffer().ok_or("Failed to create buffer")?;
     let cell_type_buffer = gl.create_buffer().ok_or("Failed to create buffer")?;
 
-    let vertices: [f32; NUM_VERTS] = cell_verts();
-    let cell_types: [f32; NUM_COLORS] = ca();
+    let total_cells: usize = UNIVERSE_WIDTH * MAX_TIME_STEPS;
+    let num_verts: usize = total_cells * 2 * 3 * 2;
+
+    let vertices: Vec<f32> = cell_verts(num_verts);
+    let cell_types = ca(UNIVERSE_WIDTH, MAX_TIME_STEPS, NUM_CELL_TYPES);
 
     // ************ VERTS
     let vert_array = {
